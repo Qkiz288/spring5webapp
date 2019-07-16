7a59913070bb8c400899aae79bf9e7fda4edc26a
Qkiz288 <qkiz288@gmail.com>
2019-07-16 09:14:54 +0200

    equals, hascode and toString methods for model

--- src/main/java/com/kkukielka/spring5webapp/model/Author.java
+++ src/main/java/com/kkukielka/spring5webapp/model/Author.java
@@ -2,6 +2,7 @@ package com.kkukielka.spring5webapp.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -28,6 +29,29 @@ public class Author {
     public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
     }
 
     public Long getId() {
